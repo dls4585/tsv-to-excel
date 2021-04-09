057a72659706dc9d1b892d497ebe6ceaf7004679
dls4585 <dls4585@naver.com>
2021-04-10 02:28:27 +0900

    뿌

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+import pandas as pd
+from tkinter import *
+from tkinter import filedialog
+
+root = Tk()
+root.withdraw()
+root.filenames = filedialog.askopenfilenames()
+l = []
+
+for file in root.filenames:
+    f = open(file, 'r')
+    while True:
+        line = f.readline()
+        if not line: break
+        row = line.split()
+        l.append(row)
+    f.close()
+
+df = pd.DataFrame(l)
+writer = pd.ExcelWriter("합친것.xlsx", options={'strings_to_url': False})
+df.to_excel(writer, index=False)
+writer.close()
+
+
+
+
+
+
 
