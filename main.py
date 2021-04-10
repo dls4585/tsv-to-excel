94a2534238c28b585bd994f06ee8998faaf444c2
dls4585 <dls4585@naver.com>
2021-04-10 17:45:14 +0900

    퍼센티지 표현

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 import pandas as pd
+import mmap
 from tkinter import *
 from tkinter import filedialog
-
-root = Tk()
-root.withdraw()
-root.filenames = filedialog.askopenfilenames()
-l = []
-
-for file in root.filenames:
-    f = open(file, 'r')
-    while True:
-        line = f.readline()
-        if not line: break
-        row = line.split()
-        l.append(row)
-    f.close()
-
-df = pd.DataFrame(l)
-writer = pd.ExcelWriter("합친것.xlsx", options={'strings_to_url': False})
-df.to_excel(writer, index=False)
-writer.close()
+from tqdm import tqdm
 
 
+def get_num_lines(file_name):
+    fp = open(file_name, "r+")
+    buf = mmap.mmap(fp.fileno(), 0)
+    lines = 0
+    while buf.readline():
+        lines += 1
+    return lines
 
 
+if __name__ == "__main__":
+    root = Tk()
+    root.withdraw()
+    root.filenames = filedialog.askopenfilenames()
+    l = []
+
+    for file in tqdm(root.filenames, desc="전체 진행률", position=0, leave=True):
+        f = open(file, 'r')
+        if not f:
+            print("cannot open " + file)
+            continue
+        # print("reading : " + file)
+        for line in tqdm(f, total=get_num_lines(file), desc=file, position=0, leave=True):
+            row = line.split()
+            l.append(row)
+        # print("done : " + file)
+        f.close()
+
+    df = pd.DataFrame(l)
+    writer = pd.ExcelWriter("Total.xlsx", options={'strings_to_url': False})
+    df.to_excel(writer, index=False)
+    writer.close()
+
+    print("complete writing to excel")
 
 
 
